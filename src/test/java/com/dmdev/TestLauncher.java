272b306a4c8915120986f69a770cfb5ebf12c57a
SevaStopAll <Sevastopallrh@yandex.ru>
2023-07-20 10:30:59 +0400

    Add DI example

--- src/test/java/com/dmdev/TestLauncher.java
+++ src/test/java/com/dmdev/TestLauncher.java
@@ -5,6 +5,7 @@ import org.junit.platform.engine.DiscoverySelector;
 import org.junit.platform.engine.discovery.DiscoverySelectors;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.TagFilter;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
@@ -20,6 +21,9 @@ public class TestLauncher {
         LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
                 .selectors(DiscoverySelectors.selectClass(UserServiceTest.class))
                 .selectors(DiscoverySelectors.selectPackage("com.dmvdev.junit.service"))
+                .filters(
+                        TagFilter.excludeTags("login")
+                )
                 .build();
         launcher.execute(request, summaryGeneratingListener);
         try (var printWriter = new PrintWriter(System.out)) {
--- src/test/java/com/dmdev/junit/service/UserServiceTest.java
+++ src/test/java/com/dmdev/junit/service/UserServiceTest.java
@@ -1,10 +1,12 @@
 package com.dmdev.junit.service;
 
 import com.dmdev.junit.dto.User;
+import com.dmdev.junit.parameresolver.UserServiceParamResolver;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.collection.IsEmptyCollection;
 import org.hamcrest.collection.IsMapContaining;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
 
 
@@ -16,11 +18,18 @@ import static org.hamcrest.collection.IsEmptyCollection.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
+@ExtendWith( {
+        UserServiceParamResolver.class
+})
 public class UserServiceTest {
 
     private static final User IVAN = User.of(1, "Ivan", "123");
     private static final User PETR = User.of(2, "Petr", "111");
     private UserService userService;
+
+    UserServiceTest(TestInfo testInfo) {
+        System.out.println();
+    }
 
     @BeforeAll
     static void init() {
@@ -28,9 +37,9 @@ public class UserServiceTest {
     }
 
     @BeforeEach
-    void prepare() {
+    void prepare(UserService userService) {
         System.out.println("Before each " + this);
-         userService = new UserService();
+         this.userService = userService;
     }
 
     @Test
@@ -45,63 +54,21 @@ public class UserServiceTest {
         System.out.println("Test2 " + this);
         userService.add(IVAN);
         userService.add(PETR);
-
         var users = userService.getAll();
-
-
         /*MatcherAssert.assertThat(users, empty());*/
         assertThat(users).hasSize(2);
         /*assertEquals(2, users.size());*/
     }
 
     @Test
-    void loginSuccessIfUserExists() {
-        userService.add(IVAN);
-
-        Optional<User> user = userService.login(IVAN.getUserName(), IVAN.getPassword());
-        assertThat(user).isPresent();
-        user.ifPresent(maybeUser -> assertThat(maybeUser).isEqualTo(IVAN));
-        /*assertTrue(user.isPresent());*/
-        /*user.ifPresent(maybeUser -> assertEquals(IVAN, maybeUser));*/
-    }
-
-    @Test
-    void loginFailIfPasswordIsNotCorrect() {
-        userService.add(IVAN);
-
-        var maybeUser = userService.login(IVAN.getUserName(), "111");
-
-        assertTrue(maybeUser.isEmpty());
-    }
-
-    @Test
-    void loginFailIfUserDoesNotExist() {
-        userService.add(IVAN);
-
-        var maybeUser = userService.login("Dummy", "111");
-
-        assertTrue(maybeUser.isEmpty());
-    }
-
-    @Test
     void usersConvertedToMapById() {
         userService.add(IVAN, PETR);
-
         Map<Integer, User> users = userService.getAllConvertedById();
-
         /*MatcherAssert.assertThat(users, IsMapContaining.hasKey(IVAN.getId()));*/
         assertAll(
                 () -> assertThat(users).containsKeys(IVAN.getId(), PETR.getId()),
                 () -> assertThat(users).containsValues(IVAN, PETR)
         );
-    }
-
-    @Test
-    void throwExceptionIfUsernameOrPasswordIsNull() {
-            assertAll(
-                    () -> assertThrows(IllegalArgumentException.class, () -> userService.login(null, "dummy")),
-                    () -> assertThrows(IllegalArgumentException.class, () -> userService.login("dummy", null))
-            );
     }
 
     @AfterEach
@@ -112,5 +79,47 @@ public class UserServiceTest {
     @AfterAll
     static void closeConnection() {
         System.out.println("After all");
+    }
+
+    @Nested
+    @DisplayName("test user login functionality")
+    @Tag("login")
+    class LoginTest {
+        @Test
+        void loginSuccessIfUserExists() {
+            userService.add(IVAN);
+
+            Optional<User> user = userService.login(IVAN.getUserName(), IVAN.getPassword());
+            assertThat(user).isPresent();
+            user.ifPresent(maybeUser -> assertThat(maybeUser).isEqualTo(IVAN));
+            /*assertTrue(user.isPresent());*/
+            /*user.ifPresent(maybeUser -> assertEquals(IVAN, maybeUser));*/
+        }
+
+        @Test
+        void loginFailIfPasswordIsNotCorrect() {
+            userService.add(IVAN);
+
+            var maybeUser = userService.login(IVAN.getUserName(), "111");
+
+            assertTrue(maybeUser.isEmpty());
+        }
+
+        @Test
+        void loginFailIfUserDoesNotExist() {
+            userService.add(IVAN);
+
+            var maybeUser = userService.login("Dummy", "111");
+
+            assertTrue(maybeUser.isEmpty());
+        }
+
+        @Test
+        void throwExceptionIfUsernameOrPasswordIsNull() {
+            assertAll(
+                    () -> assertThrows(IllegalArgumentException.class, () -> userService.login(null, "dummy")),
+                    () -> assertThrows(IllegalArgumentException.class, () -> userService.login("dummy", null))
+            );
+        }
     }
 }
